8ef618f7cbf9af03b2bdf2e744daf8cc1cdea54b
FFrabetti <frabettifil@gmail.com>
2018-07-03 16:20:29 +0200

    swag2: test per obstacle avoidance

---
+++ it.unibo.finalTask2018/srcMore/it/unibo/ctxSwag2/finaltask2018.pl
@@ -0,0 +1,12 @@
+%====================================================================================
+% Context ctxSwag2  SYSTEM-configuration: file it.unibo.ctxSwag2.finalTask2018.pl 
+%====================================================================================
+context(ctxswag2, "localhost",  "TCP", "8016" ).  		 
+%%% -------------------------------------------
+qactor( swag2pa , ctxswag2, "it.unibo.swag2pa.MsgHandle_Swag2pa"   ). %%store msgs 
+qactor( swag2pa_ctrl , ctxswag2, "it.unibo.swag2pa.Swag2pa"   ). %%control-driven 
+qactor( swag2tester , ctxswag2, "it.unibo.swag2tester.MsgHandle_Swag2tester"   ). %%store msgs 
+qactor( swag2tester_ctrl , ctxswag2, "it.unibo.swag2tester.Swag2tester"   ). %%control-driven 
+%%% -------------------------------------------
+%%% -------------------------------------------
+

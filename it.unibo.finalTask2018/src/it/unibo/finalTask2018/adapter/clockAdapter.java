8662194bcbee98dc7eac7710ab4c14fb318c9381
FFrabetti <frabettifil@gmail.com>
2018-07-05 23:06:08 +0200

    refactoring ThermoGUI e ClockGUI

--- it.unibo.finalTask2018/src/it/unibo/finalTask2018/adapter/clockAdapter.java
+++ it.unibo.finalTask2018/src/it/unibo/finalTask2018/adapter/clockAdapter.java
@@ -5,6 +5,7 @@ import it.unibo.qactors.akka.QActor;
 public class clockAdapter {
 	
 	// per requirementsAnalysis.qa
+	// -----------------------------------------------------
 	static int cont = 0;
 	
 	public static void getTime(QActor qa) {
@@ -13,30 +14,12 @@ public class clockAdapter {
 	}
 	// -----------------------------------------------------
 	
-	
 	public static void initGUI(QActor qa) {
-		new Thread(() -> {
-			try {
-				while (true) {
-					Thread.sleep(10000);
-					try {
-						clockAdapter.emitTime(qa);
-					} catch(NullPointerException npe) {
-						// do nothing
-						System.out.println("still waiting for clockGUI...");
-					}
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}).start();
-
-		it.unibo.clock.appl.Main.launch();
+		it.unibo.clock.appl.Main.launch(9,30);
 	}
 
 	public static void emitTime(QActor qa) {
 		int[] time = it.unibo.clock.appl.Main.getTime();
-
 		qa.emit("clock", "clock(" + time[0] + "," + time[1] + ")");
 	}
 	
--- it.unibo.finalTask2018/src/it/unibo/finalTask2018/adapter/thermoAdapter.java
+++ it.unibo.finalTask2018/src/it/unibo/finalTask2018/adapter/thermoAdapter.java
@@ -5,28 +5,11 @@ import it.unibo.qactors.akka.QActor;
 public class thermoAdapter {
 
 	public static void initGUI(QActor qa) {
-		new Thread(() -> {
-			try {
-				while (true) {
-					Thread.sleep(10000);
-					try {
-						thermoAdapter.emitTemp(qa);
-					} catch(NullPointerException npe) {
-						// do nothing
-						System.out.println("still waiting for thermoGUI...");
-					}
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} 
-		}).start();
-
-		it.unibo.thermo.appl.Main.launch();
+		it.unibo.thermo.appl.Main.launch(20);
 	}
 
 	public static void emitTemp(QActor qa) {
 		double t = it.unibo.thermo.appl.Main.getT();
-
 		qa.emit("temperature", "temperature(" + t + ")");
 	}
 

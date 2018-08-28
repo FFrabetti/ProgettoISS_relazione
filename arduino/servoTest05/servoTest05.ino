896e637de472d4f2e6085eeed8030d6492fedaba
FFrabetti <frabettifil@gmail.com>
2018-08-28 22:37:47 +0200

    test ponte H: motore a 2 direzioni

--- arduino/servoTest05/servoTest05.ino
+++ arduino/servoTest05/servoTest05.ino
@@ -2,28 +2,60 @@
 
 int const SRV_PIN = 9;
 int const MOTOR_PIN = 8;
+int const FORW_PIN = 7;
+int const RETRO_PIN = 6;
+int const LED_PIN = 12;
 
 Servo myServo;
 
 void setup() {
-  myServo.attach(SRV_PIN);
-  pinMode(MOTOR_PIN, OUTPUT);
-  
-  Serial.begin(9600);
+	myServo.attach(SRV_PIN);
+	pinMode(MOTOR_PIN, OUTPUT);
+	pinMode(FORW_PIN, OUTPUT);
+	pinMode(RETRO_PIN, OUTPUT);
+	pinMode(LED_PIN, OUTPUT);
+
+	Serial.begin(9600);
 }
 
 void loop() {
-  int n;
-  int angle = 0;
-  if(Serial.available()){
-    n = Serial.read() - '0';
-    angle = map(n, 0, 9, 0, 179);
-    myServo.write(angle);
-    delay(15);
+	delay(200);
+	// int n;
+	// int angle = 0;
+	
+	if(Serial.available()) {
+    // n = Serial.read() - '0';
+    // angle = map(n, 0, 9, 0, 179);
+    // myServo.write(angle);
+	
+		char n = Serial.read();
+		if(n=='w')
+			motorForward();
+		else if(n=='s')
+			motorBackward();
+		else if(n=='h')
+			motorStop();
+	}
+}
 
-    if(n==0)
-      digitalWrite(MOTOR_PIN, LOW);
-    else
-      digitalWrite(MOTOR_PIN, HIGH);
-  }
+void motorForward() {
+	motorStart(1);
+}
+
+void motorBackward() {
+	motorStart(-1);
+}
+
+void motorStart(int dir) {
+	digitalWrite(MOTOR_PIN, HIGH);
+	digitalWrite(LED_PIN, HIGH);
+	digitalWrite(FORW_PIN, dir>0 ? HIGH : LOW);
+	digitalWrite(RETRO_PIN, dir>0 ? LOW : HIGH);
+}
+
+void motorStop() {
+	digitalWrite(MOTOR_PIN, LOW);
+	digitalWrite(LED_PIN, LOW);
+	digitalWrite(FORW_PIN, LOW);
+	digitalWrite(RETRO_PIN, LOW);
 }
